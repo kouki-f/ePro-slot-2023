acebd78bc235f58d6c76affc11d5f4fd0fee0945
kouki <koukifujii0512@gmail.com>
2023-09-24 12:28:03 +0900

    feat: 二桁のセグメントLEDを制御するプログラム

---
+++ src/ArduinoMega/MyLibrary_ArduinoMega/TwoDigitsSegmentLed.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class TwoDigitsSegmentLed{
+  private:
+    int m_pinA1, m_pinB1, m_pinC1,
+        m_pinD1, m_pinE1, m_pinF1,
+        m_pinG1, m_pinA2, m_pinB2,
+        m_pinC2, m_pinD2, m_pinE2,
+        m_pinF2, m_pinG2;
+    int m_num;
+    int m_tens, m_ones;
+    const int kTens_place = 1;
+    const int kOnes_place = 2;
+
+    int GetTensPlace(int n);  //十の位の数字を取得
+    int GetOnesPlace(int n);  //一の位の数字を取得
+    void displayZero(int place);
+    void displayOne(int place);
+    void displayTwo(int place);
+    void displayThree(int place);
+    void displayFour(int place);
+    void displayFive(int place);
+    void displaySix(int place);
+    void displaySeven(int place);
+    void displayEight(int place);
+    void displayNine(int place);
+    void setHIGH(int pin);
+    void setLOW(int pin);
+
+  public:
+    TwoDigitsSegmentLed(int pinA1, int pinB1, int pinC1,
+                        int pinD1, int pinE1, int pinF1,
+                        int pinG1, int pinA2, int pinB2,
+                        int pinC2, int pinD2, int pinE2,
+                        int pinF2, int pinG2);
+
+    void DisplayNumber(int n);
+    void TurnOffAllLed();
+};

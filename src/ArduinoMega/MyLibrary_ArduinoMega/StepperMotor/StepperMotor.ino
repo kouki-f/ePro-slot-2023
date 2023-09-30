8b96a1f54fff7c13a44c202663b4583e8b038c19
kouki <koukifujii0512@gmail.com>
2023-09-30 16:47:34 +0900

    feat: ステッピングモータのテストプログラムを作成

--- src/ArduinoMega/MyLibrary_ArduinoMega/StepperMotor/StepperMotor.ino
+++ src/ArduinoMega/MyLibrary_ArduinoMega/StepperMotor/StepperMotor.ino
@@ -1,22 +1,15 @@
 #include <Stepper.h>
 
-int around_step = 200;  //1回転ステップ数
+int around_step = 200;  // 1回転ステップ数
 int step = 10;
-int speed = 50;  //50rpm
-int motor_pin1 = 10;
-int motor_pin2 = 11;
+int speed =80;   // 回転スピード(rpm）
 
-Stepper myStepper(around_step, motor_pin1, motor_pin2);
+Stepper stepper(around_step, 4,5,6,7);
 
 void setup() {
-  // put your setup code here, to run once:
-  myStepper.setSpeed(speed);
+  stepper.setSpeed(speed);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  myStepper.step(100);
-  delay(500);
-  myStepper.step(-100);
-  delay(500);
+  stepper.step(step);
 }

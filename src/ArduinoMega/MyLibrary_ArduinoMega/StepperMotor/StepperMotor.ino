18f1dc4eb4226c0811d6681020e3e8566b14ef72
kouki <koukifujii0512@gmail.com>
2023-10-01 01:23:07 +0900

    test: ボタンを押したときに止める動作を確かめた

--- src/ArduinoMega/MyLibrary_ArduinoMega/StepperMotor/StepperMotor.ino
+++ src/ArduinoMega/MyLibrary_ArduinoMega/StepperMotor/StepperMotor.ino
@@ -4,25 +4,12 @@ int speed = 80;
 StepperMotor motor1(speed, 4, 5, 6, 7);
 
 void setup() {
-  motor1.rotateAngle(90);
-  delay(500);
-  motor1.rotateAngle(90);
-  delay(500);
-  motor1.rotateAngle(90);
-  delay(500);
-  motor1.rotateAngle(90);
-  delay(500);
-
-  motor1.rotateAngle(-90);
-  delay(500);
-  motor1.rotateAngle(-90);
-  delay(500);
-  motor1.rotateAngle(-90);
-  delay(500);
-  motor1.rotateAngle(-90);
-  delay(500);
+  pinMode(10, INPUT_PULLUP);
 }
 
 void loop() {
-  
+  while(digitalRead(10)){
+    motor1.rotateAngle(5);    
+  }
+  while(1){}
 }

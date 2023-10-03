b046d97b416bbe635f21a9b111a5f0137f6c49de
kouki <koukifujii0512@gmail.com>
2023-10-04 01:36:27 +0900

    feat: ピン管理の配列を作成、ライブラリのインスタンス化
    ファイルを移動

--- src/ArduinoMega/MyLibrary_ArduinoMega/GameController/GameController.cpp
+++ src/ArduinoMega/MyLibrary_ArduinoMega/GameController/GameController.cpp
@@ -1,6 +1,7 @@
+#include "StepperMotor.h"
 #include "GameController.h"
-#include <Arduino.h>
+#include "Arduino.h"
 
-GameController::GameController(){
+GameController::GameController(int* array, int size):motor1(array[0], array[1], array[2], array[3], array[4]){
   
 }
--- src/ArduinoMega/MyLibrary_ArduinoMega/GameController/GameController.h
+++ src/ArduinoMega/MyLibrary_ArduinoMega/GameController/GameController.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "StepperMotor.h"
+#include "OneDigitSegmentLed.h"
+#include "TwoDigitsSegmentLed.h"
+#include "SoundEffect.h"
+#include "ButtonWithLed.h"
+#include "CoinSelector.h"
 
 class GameController{
 private:
-  /* data */
+  StepperMotor motor1, motor2, motor3;
+  OneDigitSegmentLed oneDigitSeg;
+  TwoDigitsSegmentLed twoDigitsSeg;
+  SoundEffect SE;
+  ButtonWithLed startBN, stopBN1, stopBN2, stopBN3, betBN, payBN;
+  CoinSelector coinSelector;
 public:
-  GameController(/* args */);
+  GameController(int* array, int size);
+  void StepperMotor1Init(int speed, int pinA1, int pinA2, int pinB1, int pinB2);
+  void test();
 };

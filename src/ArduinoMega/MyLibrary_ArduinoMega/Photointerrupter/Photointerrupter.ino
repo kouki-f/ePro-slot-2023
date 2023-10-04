9ff7150da5563d403371f34bc7fe358263456a46
kouki <koukifujii0512@gmail.com>
2023-10-04 20:36:58 +0900

    refactor: 可読性のため一部書き換え

--- src/ArduinoMega/MyLibrary_ArduinoMega/Photointerrupter/Photointerrupter.ino
+++ src/ArduinoMega/MyLibrary_ArduinoMega/Photointerrupter/Photointerrupter.ino
@@ -3,13 +3,11 @@
 Photointerrupter photoSensor1(2);
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(photoSensor1.readPhoto()==HIGH){
+  if(photoSensor1.readPhoto()){
     digitalWrite(3, HIGH);
   }else{
     digitalWrite(3, LOW);

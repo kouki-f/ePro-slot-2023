b7a1b658f110cf3b65568387ce99672126691233
kouki <koukifujii0512@gmail.com>
2023-10-05 16:42:26 +0900

    feat: inoファイルに基本プログラムを作成

--- src/ArduinoNano_LedTape/ArduinoNano_LedTape.ino
+++ src/ArduinoNano_LedTape/ArduinoNano_LedTape.ino
@@ -1,9 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(Serial.available()>0){
+    int val = Serial.read();
+    if(val == '1'){
 
+    }else if (val == '2') {
+
+    }
+  }
 }

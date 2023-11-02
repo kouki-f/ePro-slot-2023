ff1d40bc40a960bf0f0c38cc5b7cf27f4f710942
kouki <koukifujii0512@gmail.com>
2023-11-02 11:58:25 +0900

    feat: test program

--- src/ArduinoMega/MyLibrary_ArduinoMega/Photointerrupter/Photointerrupter.ino
+++ src/ArduinoMega/MyLibrary_ArduinoMega/Photointerrupter/Photointerrupter.ino
@@ -1,15 +1,18 @@
 #include "Photointerrupter.h"
 
-Photointerrupter photoSensor1(2);
+Photointerrupter photoSensor1(53);
 
 void setup() {
+  Serial.begin(9600);
   pinMode(3, OUTPUT);
 }
 
 void loop() {
   if(photoSensor1.readPhoto()){
-    digitalWrite(3, HIGH);
+    //digitalWrite(3, HIGH);
+    Serial.println("HIGH!");
   }else{
-    digitalWrite(3, LOW);
+    // digitalWrite(3, LOW);
+    Serial.println("LOW!");
   }
 }
